8609e3f119772bc6e9195c59d7400cf9efebb002
zhushunsheng <503305196@qq.com>
2020-11-20 13:21:12 +0800

    模拟测试数据

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>lazy-cascader</h1>
     <div class="demo">
-      <lazy-cascader v-model="value" filterable :props="props"></lazy-cascader>
+      <div class="demo-title">单选</div>
+      <div>
+        <lazy-cascader
+          v-model="value"
+          filterable
+          :props="props"
+        ></lazy-cascader>
+      </div>
     </div>
-    <div class="doc">
-      <div class="doc-title"></div>
+    <div class="demo">
+      <div class="demo-title">单选不关联</div>
+      <div>
+        <lazy-cascader
+          v-model="value3"
+          filterable
+          :props="props3"
+        ></lazy-cascader>
+      </div>
+    </div>
+    <div class="demo">
+      <div class="demo-title">多选</div>
+      <div>
+        <lazy-cascader
+          v-model="value2"
+          filterable
+          :props="props2"
+        ></lazy-cascader>
+      </div>
+    </div>
+    <div class="demo">
+      <div class="demo-title">多选不关联</div>
+      <div>
+        <lazy-cascader
+          v-model="value4"
+          filterable
+          :props="props4"
+        ></lazy-cascader>
+      </div>
     </div>
   </div>
 </template>
@@ -19,13 +53,80 @@ export default {
   },
   data() {
     return {
-      value: [1, 2, 3, 4],
+      current: [],
+      options: [
+        {
+          id: 1,
+          name: "服装",
+          children: [
+            {
+              id: 3,
+              name: "男装",
+              children: [
+                {
+                  id: 7,
+                  name: "T恤"
+                }
+              ]
+            },
+            {
+              id: 4,
+              name: "女装"
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "数码",
+          children: [
+            {
+              id: 5,
+              name: "手机"
+            },
+            {
+              id: 6,
+              name: "电脑"
+            }
+          ]
+        }
+      ],
+      value: [1, 3, 7],
       props: {
         multiple: false,
         checkStrictly: false,
-        value: "v",
-        label: "l",
-        leaf: "Leaf",
+        value: "id",
+        label: "name",
+        leaf: "leaf",
+        lazyLoad: this.lazyLoad,
+        lazySearch: this.lazySearch
+      },
+      value2: [],
+      props2: {
+        multiple: true,
+        checkStrictly: false,
+        value: "id",
+        label: "name",
+        leaf: "leaf",
+        lazyLoad: this.lazyLoad,
+        lazySearch: this.lazySearch
+      },
+      value3: [],
+      props3: {
+        multiple: false,
+        checkStrictly: true,
+        value: "id",
+        label: "name",
+        leaf: "leaf",
+        lazyLoad: this.lazyLoad,
+        lazySearch: this.lazySearch
+      },
+      value4: [[1], [1, 3]],
+      props4: {
+        multiple: true,
+        checkStrictly: true,
+        value: "id",
+        label: "name",
+        leaf: "leaf",
         lazyLoad: this.lazyLoad,
         lazySearch: this.lazySearch
       }
@@ -34,38 +135,76 @@ export default {
   methods: {
     //加载级联的方法
     lazyLoad(nodeValue, resolve) {
-      if (nodeValue < 3) {
-        setTimeout(() => {
-          resolve([
-            {
-              v: nodeValue + 1,
-              l: "类目" + (nodeValue + 1),
-              Leaf: false
-            }
-          ]);
-        }, 300);
-      } else {
-        setTimeout(() => {
-          resolve([
-            {
-              v: nodeValue + 1,
-              l: "类目" + (nodeValue + 1),
-              Leaf: true
-            }
-          ]);
-        }, 300);
-      }
+      setTimeout(() => {
+        resolve(this.getCateList(nodeValue));
+      }, 200);
     },
     lazySearch(queryString, callback) {
-      callback([
-        {
-          v: [1, 2, 3, 4],
-          l: ["类目1", "类目2", "类目3", "类目4"]
+      setTimeout(() => {
+        callback([
+          {
+            id: [1, 3, 7],
+            name: ["服装", "男装", "T恤"]
+          },
+          {
+            id: [2, 5],
+            name: ["数码", "手机"]
+          }
+        ]);
+      }, 200);
+    },
+    //获取节点数据
+    getCateList(parent) {
+      console.log(parent);
+      if (parent == 0) {
+        return this.options.map(item => {
+          let obj = {
+            id: item.id,
+            name: item.name,
+            leaf: true
+          };
+          if (item.children && item.children.length > 0) {
+            obj.leaf = false;
+          }
+          return obj;
+        });
+      } else {
+        this.current = [];
+        this.findCate(parent, this.options);
+        return this.current.map(item => {
+          let obj = {
+            id: item.id,
+            name: item.name,
+            leaf: true
+          };
+          if (item.children && item.children.length > 0) {
+            obj.leaf = false;
+          }
+          return obj;
+        });
+      }
+    },
+    findCate(parent, children) {
+      for (let i = 0; i < children.length; i++) {
+        if (parent == children[i].id) {
+          this.current = children[i].children;
+        } else {
+          if (children[i].children) {
+            this.findCate(parent, children[i].children);
+          }
         }
-      ]);
+      }
     }
   }
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+#app {
+  width: 800px;
+  margin: 100px auto;
+}
+.demo {
+  margin-top: 10px;
+}
+</style>
